bfa19d0340086c4a0ceca7a1a75c1f001091e49e
Larry Crochet <larry.crochet@libertyits.com>
2020-07-23 11:40:06 -0400

    remove debug logs

--- conformance-unifier/src/main/java/gov/va/api/health/conformance/unifier/Application.java
+++ conformance-unifier/src/main/java/gov/va/api/health/conformance/unifier/Application.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -16,7 +15,6 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({AmazonS3ClientServiceConfig.class})
-@CommonsLog
 public class Application implements ApplicationRunner {
 
   /** Optional argument to associate metadata with the generated S3 object. */
@@ -45,8 +43,6 @@ public class Application implements ApplicationRunner {
     }
     // Check count of required non option args.
     if (argList.size() < ArgEnum.values().length) {
-      log.error("argList:" + argList);
-      argList.forEach(arg -> log.error("arg:" + arg));
       throw new IllegalArgumentException(
           "Invalid number of arguments.  Expected minimum count " + ArgEnum.values().length + ".");
     }
@@ -57,13 +53,12 @@ public class Application implements ApplicationRunner {
     Map<String, String> metadataMap = new HashMap<>();
     if (metadataValueList != null) {
       try {
-        for (String metadataKeyValueString : metadataValueList) {
-          final String[] metadataKeyValueArray = metadataKeyValueString.split("\\s*,\\s*");
-          for (String keyValueString : metadataKeyValueArray) {
-            final String[] keyValuePair = keyValueString.split("=", 2);
-            metadataMap.put(keyValuePair[0], keyValuePair[1]);
-          }
-        }
+          metadataValueList.stream().map(metadataKeyValueString -> metadataKeyValueString.split("\\s*,\\s*")).forEachOrdered(metadataKeyValueArray -> {
+              for (String keyValueString : metadataKeyValueArray) {
+                  final String[] keyValuePair = keyValueString.split("=", 2);
+                  metadataMap.put(keyValuePair[0], keyValuePair[1]);
+              }
+          });
       } catch (Exception e) {
         throw new IllegalArgumentException("Invalid metadata argument: " + e.getMessage());
       }
