38127e16d48c0103afae710d4b56f2e5d5726e41
Kevin Cranmer <51764175+KevinCranmer@users.noreply.github.com>
2023-08-22 08:08:09 -0500

    Parent Version Bump to 16 (#84)
    * Parent Version Bump to 16
    
    * RIP pteracuda

--- conformance-unifier/src/main/java/gov/va/api/health/conformance/unifier/fhir/dstu2/CurrentPublicationDateDstu2ConformanceStatementProperties.java
+++ conformance-unifier/src/main/java/gov/va/api/health/conformance/unifier/fhir/dstu2/CurrentPublicationDateDstu2ConformanceStatementProperties.java
@@ -1,9 +1,9 @@
 package gov.va.api.health.conformance.unifier.fhir.dstu2;
 
 import gov.va.api.health.informational.dstu2.conformance.ConformanceStatementProperties;
+import jakarta.annotation.PostConstruct;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import javax.annotation.PostConstruct;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
